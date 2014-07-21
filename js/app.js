a997fc1b7586d914c841e6b093ef27be8138cd39
Solo206 <jsolk206@gmail.com>
2014-07-21 10:57:07 -0700

    clicking will stop theme music

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	gameDemo();
 });
 function playHadouken () {
+	$('#ryu-theme')[0].stop();
 	$('#hadouken-sound')[0].volume=0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
