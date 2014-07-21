770201aa1e87447c020efa4c4a60146d07828786
Solo206 <jsolk206@gmail.com>
2014-07-21 10:38:42 -0700

    upload streetfighter w/hadouken
    Moves with hadouken

--- js/app.js
+++ js/app.js
@@ -2,9 +2,37 @@ $(document).ready(function(){
 	$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	});
-	$('.ryu').mouseleave(function(){
+	})
+	.mouseleave(function(){
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
+	})
+	.mousedown(function(){
+		// console.log('mousedown');
+		playHadouken();
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left':'300px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left','-155px');
+		});
+		//play hadouken sound
+	})
+	.mouseup(function(){
+		// console.log('mouseup');
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+		//ryu goes back to ready position
 	});
 });
+function playHadouken () {
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
